21e2050509f55c6ed578fab79bb62a354e9c35ce
7N 192.168.207.15 <carsasgar@alu.edu.gva.es>
2026-02-03 15:54:30 +0100

    feat: Añadir dependencias de Room y KSP
    Se añaden las dependencias necesarias para implementar la base de datos Room, incluyendo el procesador de anotaciones KSP.
    
    Cambios específicos:
    - Se agregan las librerías de Room (runtime, ktx, compiler) y el plugin de KSP en los archivos `build.gradle.kts` y `libs.versions.toml`.
    - Se actualiza la configuración del proyecto para usar Java 21 y se establece el `jvmTarget` a JVM 11.
    - `MainActivity` ahora hereda de `AppCompatActivity`.
    - Se ajusta el manejo de eventos `onBack` y `onSearchClick` en `HomeRoute`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
 
     alias(libs.plugins.jetbrains.kotlin.serialization)
+    alias(libs.plugins.google.devtools.ksp)
 }
 
 android {
@@ -77,6 +78,9 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     // Lifecycle utilities for Compose
     implementation(libs.androidx.lifecycle.runtime.compose)
+    // Lifecycle dependencies
+    //implementation(libs.androidx.lifecycle.livedata.ktx)
+    //implementation(libs.androidx.activity.ktx)
     // Saved state module for ViewModel
     implementation(libs.androidx.lifecycle.viewmodel.savedstate)
     // koin
@@ -93,4 +97,11 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     testImplementation("com.squareup.okhttp3:mockwebserver3:5.3.0")
     debugImplementation("com.squareup.okhttp3:mockwebserver3:5.3.0")
+    // AndroidX dependencies
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    // Room dependencies
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
 }
