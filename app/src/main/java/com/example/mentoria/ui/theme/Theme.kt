04586b537e3e26b9c46ea5f9b8b5153e7c847ee3
Carolina Sastre <carolsaga02@gmail.com>
2026-02-07 20:14:58 +0100

    ksp instalado

--- app/src/main/java/com/example/mentoria/ui/theme/Theme.kt
+++ app/src/main/java/com/example/mentoria/ui/theme/Theme.kt
@@ -33,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun Mentoria(
+fun MentoriaTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
