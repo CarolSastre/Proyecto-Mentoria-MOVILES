98e685cc10cf5ab7166ba3b360583121397a590b
Carolina Sastre <carolsaga02@gmail.com>
2026-02-16 17:17:43 +0100

    archivo suelto

---
+++ app/src/main/java/com/example/mentoria/core/presentation/screens/usuariodetails/UsuarioDetailsUiState.kt
@@ -0,0 +1,7 @@
+package com.example.mentoria.core.presentation.screens.usuariodetails
+
+import com.example.mentoria.core.domain.model.Usuario
+
+data class UsuarioDetailsUiState (
+    val usuario: Usuario
+)
