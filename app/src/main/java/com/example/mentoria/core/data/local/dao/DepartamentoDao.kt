8d314a51c455a15cf422823354b8cc2ffaa8024b
Carolina Sastre <carolsaga02@gmail.com>
2026-02-25 04:02:22 +0100

    Areglar entity

--- app/src/main/java/com/example/mentoria/core/data/local/dao/DepartamentoDao.kt
+++ app/src/main/java/com/example/mentoria/core/data/local/dao/DepartamentoDao.kt
@@ -4,22 +4,22 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.Companion.REPLACE
 import androidx.room.Query
-import com.example.mentoria.core.data.local.entities.HorarioEntity
+import com.example.mentoria.core.data.local.entities.DepartamentoEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface DepartamentoDao {
     @Query("SELECT * FROM departamentos WHERE id = :id")
-    fun getDepartamentoById(id: String): Flow<HorarioEntity>
+    fun getDepartamentoById(id: String): Flow<DepartamentoEntity>
 
     @Query("SELECT * FROM departamentos WHERE nombre LIKE :nombre")
-    fun getDepartamentoByNombre(nombre: String): Flow<HorarioEntity>
+    fun getDepartamentoByNombre(nombre: String): Flow<DepartamentoEntity>
 
     @Query("SELECT * FROM departamentos")
-    fun getAllDepartamento(): Flow<List<HorarioEntity>>
+    fun getAllDepartamento(): Flow<List<DepartamentoEntity>>
 
     @Insert(onConflict = REPLACE)
-    suspend fun insertAll(registros: List<HorarioEntity>)
+    suspend fun insertAll(registros: List<DepartamentoEntity>)
 
     @Query("DELETE FROM departamentos")
     suspend fun deleteAll()
